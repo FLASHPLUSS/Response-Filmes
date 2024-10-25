29f15aaf501fcb2f95e2934ae040aaed35c235f2
DEVELOPS MAX <132115571+FLASHPLUSS@users.noreply.github.com>
2024-10-25 10:05:41 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -4,26 +4,26 @@ from cachetools import TTLCache
 
 app = FastAPI()
 
-# Configuração da URL fixa do Dropbox
+# URL do Dropbox para acessar o arquivo JSON com filmes e séries
 DROPBOX_URL = "https://dl.dropboxusercontent.com/s/v2fuahx8uxcar2edrkk8m/Filmes_Series.json?rlkey=mjw8j0iw5047eofkcm2wkqwbo&st=do83pnno&dl=1"
 
 # Sessão de requisições persistente e cache com expiração
 session = requests.Session()
-cache = TTLCache(maxsize=1, ttl=300)  # Cache com 1 item e 5 minutos de expiração (300 segundos)
+cache = TTLCache(maxsize=1, ttl=300)  # Cache de 5 minutos
 
 @app.get("/filmes")
 async def obter_filmes():
-    # Verifica o cache
+    # Verifica se os dados estão no cache
     if "filmes_data" in cache:
         return cache["filmes_data"]
 
     try:
-        # Faz a requisição ao link direto do Dropbox usando a sessão persistente
+        # Faz a requisição ao link do Dropbox usando a sessão persistente
         response = session.get(DROPBOX_URL)
         response.raise_for_status()
         filmes_data = response.json()  # Carrega o conteúdo JSON
 
-        # Armazena no cache para respostas mais rápidas nas próximas requisições
+        # Armazena no cache para respostas rápidas nas próximas requisições
         cache["filmes_data"] = filmes_data
 
     except requests.exceptions.RequestException as e:
